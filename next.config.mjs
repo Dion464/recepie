774d362b14e6c7caa1c969de985c698eedbae695
Dion464 <curridion31@gmail.com>
2025-01-06 11:22:29 +0100

    initiale commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Ensures the App Router is enabled
-  },
-  reactStrictMode: true, // Optional: Enforces best practices
+  reactStrictMode: true, // Enforces best practices (optional)
   env: {
     NEXT_PUBLIC_SPOONACULAR_API_KEY: process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY, // Ensures the API key is exposed
   },
